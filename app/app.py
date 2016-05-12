8864b3d9dc840cd9378f084989bcbc7d26147bbd
Andre Macedo <andrefrmacedo@gmail.com>
2016-05-12 17:44:44 +0100

    Basic web interface

--- app/app.py
+++ app/app.py
@@ -1,8 +1,6 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return "Hello World"
+app.register_blueprint(views)
---
+++ app/views/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__, url_prefix="")
+
+
+@views.route('/')
+def index():
+    return render_template('index.html')
+
+
+@views.route('/mealstandard')
+def meal_standard():
+    return render_template('mealstandard.html')
+
+
+@views.route('/mealpersonal')
+def meal_personal():
+    return render_template('mealpersonal.html')
+
+
+@views.route('/background')
+def background():
+    return render_template('background.html')
