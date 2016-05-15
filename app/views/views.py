c589e91e7fb525c6769cb9a460877f9e7cc870cb
Andre Macedo <andrefrmacedo@gmail.com>
2016-05-15 02:36:52 +0100

    Completed mealtime standard form action

--- app/views/views.py
+++ app/views/views.py
@@ -30,14 +30,31 @@ def get_background_dose():
     weight = request.form['weight']
     data = n_voter(0, [weight])
 
-    return render_template('response.html', data=data, type='Background Dose', details=False)
+    return render_template('response.html', data=data, type='Background Dose')
 
 
 @views.route('/mealtimeStandard', methods=['POST'])
 def get_mealtime_standard():
-    return render_template('index.html')
+    totalCarbs = request.form['totalCarbs']
+    totalGrams = request.form['totalGrams']
+    bloodSugar = request.form['bloodSugar']
+    targetSugar = request.form['targetSugar']
+    sens = request.form['sens']
+
+    data = n_voter(1, [totalCarbs, totalGrams, bloodSugar, targetSugar, sens])
+
+    return render_template('response.html', data=data, type='Mealtime Dose Standard')
 
 
 @views.route('/mealtimePersonal', methods=['POST'])
 def get_mealtime_personal():
-    return render_template('index.html')
+
+    totalCarbs = request.form['totalCarbs']
+    totalGrams = request.form['totalGrams']
+    bloodSugar = request.form['bloodSugar']
+    targetSugar = request.form['targetSugar']
+    # missing some args
+
+    data = n_voter(2, [totalCarbs, totalGrams, bloodSugar, targetSugar])
+
+    return render_template('index.html', data=data, type='Mealtime Dose Personal')
